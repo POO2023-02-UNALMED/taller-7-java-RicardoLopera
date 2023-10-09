a594579414a86342bc2f0bf3d3670a3066c7fc1c
RicardoLopera <rloperav@unal.edu.co>
2023-10-08 21:39:27 -0500

    canela

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -1,5 +1,38 @@
 package comunicacion;
 
-public class Fabula {
+public class Fabula extends Escrito {
 
+	private String ensenanza;
+	private String interpretacion;
+	
+	public Fabula (String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.ensenanza = ensenanza;
+		this.interpretacion = interpretacion;
+	}
+	public String interpretacion() {
+		return getInterpretacion();
+	}
+	public String toString() {
+		String informacion = getOrigen()+ "\n"+getTitulo()+"\n"+ getAutor()+ "\n"+getPaginas()+"\n"+ getEnsenanza();
+		return informacion;
+	}
+	public int palabrasTotales(int paginas) {
+		int numeroPalabras = getPaginas()*paginas*1;
+		return numeroPalabras;
+	}
+	
+	public String getEnsenanza() {
+		return ensenanza;
+	}
+	public void setEnseñanza(String ensenanza) {
+		this.ensenanza = ensenanza;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
 }
